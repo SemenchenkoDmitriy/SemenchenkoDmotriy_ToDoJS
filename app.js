14167c280f4eecb4bc84549960d6d29f85fcf4d8
Dmitriy <d.semenchenko@tg.dunice.net>
2024-06-21 10:40:43 +0300

    add array todos to app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const input = document.querySelector('.todo-app input[type="text"]');
+    const addButton = document.querySelector('.add-button');
+    const todoList = document.querySelector('.todo-app ul');
+
+    let todos = [];
+
+    function renderTodos() {
+
+    }
+
+});
